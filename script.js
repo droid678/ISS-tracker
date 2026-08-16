5dc7abc7a9bb6c06391aa782956f1e759b503de4
droid678 <hidden.history1543@gmail.com>
2026-08-16 23:41:36 +0530

    Fix map bounds and zoom, plot ISS marker

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const map = L.map('map').setView([0, 0], 2);
+const map = L.map('map', {
+    minZoom: 2,
+}).setView([0, 0], 2);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 fetch(`http://api.open-notify.org/iss-now.json`)
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    const lat = parseFloat(data.iss_position.latitude);
+    const lon = parseFloat(data.iss_position.longitude);
+
+    L.marker([lat, lon]).addTo(map)
+      .bindPopup('The ISS is here!')
+      .openPopup();
+
+    map.setView([lat, lon], 4);    console.log(data);
   });
