3ced91a10b2400a96c353473189e7f1410bf4c70
droid678 <hidden.history1543@gmail.com>
2026-08-16 23:08:03 +0530

    Add Leaflet map

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const map = L.map('map').setView([0, 0], 2);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
 fetch(`http://api.open-notify.org/iss-now.json`)
   .then(response => response.json())
   .then(data => {
