cb586283091013d996f455acde0335e4f7152b76
droid678 <hidden.history1543@gmail.com>
2026-08-16 23:59:02 +0530

    Add live position updates every 5 seconds

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 const map = L.map('map', {
-    minZoom: 2,
+    minZoom: 2
 }).setView([0, 0], 2);
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
-fetch(`http://api.open-notify.org/iss-now.json`)
-  .then(response => response.json())
-  .then(data => {
-    const lat = parseFloat(data.iss_position.latitude);
-    const lon = parseFloat(data.iss_position.longitude);
 
-    L.marker([lat, lon]).addTo(map)
-      .bindPopup('The ISS is here!')
-      .openPopup();
+let issMarker = null;
 
-    map.setView([lat, lon], 4);    console.log(data);
-  });
+function updateISSPosition() {
+  fetch('http://api.open-notify.org/iss-now.json')
+    .then(response => response.json())
+    .then(data => {
+      const lat = parseFloat(data.iss_position.latitude);
+      const lon = parseFloat(data.iss_position.longitude);
+
+      if (issMarker === null) {
+        issMarker = L.marker([lat, lon]).addTo(map)
+          .bindPopup('The ISS is here!');
+        map.setView([lat, lon], 4);
+      } else {
+        issMarker.setLatLng([lat, lon]);
+      }
+
+      console.log('Updated position:', lat, lon);
+    });
+}
+
+updateISSPosition();
+setInterval(updateISSPosition, 5000);
